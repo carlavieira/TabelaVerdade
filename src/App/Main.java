a61147f907064d684026ed3a867b73b45481e17d
arthur.branco <arthurgbranco2@gmail.com>
2019-10-13 16:30:33 -0300

    Added conectives methods with exceptions

--- src/App/Main.java
+++ src/App/Main.java
@@ -5,8 +5,9 @@ import utils.StringsUtil;
 public class Main {
     public static void main (String args[]){
         try{
-            String equation = " q^p>r =s";
+            String equation = " q+p*r .s>=!";
             System.out.println(StringsUtil.propositionList(equation).toString());
+            System.out.println(StringsUtil.conectivesList(equation).toString());
         }catch (Exception e){
             e.printStackTrace();
         }
