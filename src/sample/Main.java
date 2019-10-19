4bcee211ee8811e1d2fa677c21879b6fa1e3e173
Carla Vieira <carlavieira@Carlas-MacBook-Air.local>
2019-10-19 16:09:26 -0300

    botões

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculadora Tabela Verdade");
+        primaryStage.setScene(new Scene(root, 600, 400));g
         primaryStage.show();
     }
 
