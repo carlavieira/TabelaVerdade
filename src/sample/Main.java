4f94d0ea79ebd221ec7c8126874ab78b4fc6430e
Carla Vieira <carlavieira@Carlas-MacBook-Air.local>
2019-10-19 16:20:35 -0300

    ids

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Calculadora Tabela Verdade");
-        primaryStage.setScene(new Scene(root, 600, 400));g
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
