cd8e5e335c5a408cecf2fe49d3ff0ff2fc39e905
Daniel Souza <daniellyncon@hotmail.com>
2019-10-27 16:38:49 -0300

    first commit

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,15 +1,132 @@
 package App;
 
+import Objects.TabelaVerdade;
+import javafx.geometry.Pos;
 import utils.StringsUtil;
 
-public class Main {
-    public static void main(String args[]) {
-        try {
-            String equation = "(((!(q+p))*r)=s)";
-            System.out.println(StringsUtil.propositionList(equation).toString());
-            System.out.println(StringsUtil.conectivesList(equation).toString());
-        } catch (Exception e) {
-            e.printStackTrace();
+//public class Main {
+//        public static void main(String args[]) {
+//        try {
+//            String equation = "(((!(q+p))*r)=s)";
+//            System.out.println(StringsUtil.propositionList(equation).toString());
+//            System.out.println(StringsUtil.conectivesList(equation).toString());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//    private static void imprimeValoresDaTabela(String frase, String[][] tabela) {
+//        for (int i = 0; i < frase.length(); i++) {
+//            for (int j = 0; j < 17; j++) {
+//                System.out.print(tabela[i][j] + " ");
+//                System.out.println("I :"+i+" J" +j);
+//            }
+//            System.out.println("");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        String fraseLogica = "pvq∧(¬pvr)∧s";
+//        String fraseLogica2 = "¬(p∧q∧(r∧p∧(pvr)))";
+//
+//        TabelaVerdade tv = new TabelaVerdade(fraseLogica);
+//
+//        imprimeValoresDaTabela(fraseLogica, tv.retornaMatrizLogica());
+//    }
+//}
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import java.util.Random;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        String fraseLogica = "pvq∧(¬pvr)∧s";
+        String fraseLogica2 = "¬(p∧q∧(r∧p∧(pvr)))";
+
+        TabelaVerdade tv = new TabelaVerdade(fraseLogica);
+
+        String[][] strings = tv.retornaMatrizLogica();
+        System.out.println(strings.length);
+        System.out.println(strings[0].length);
+        String[][] strings2 = new String[12][17];
+
+
+        for (int i = 0; i < strings2.length; i++) {
+
+            strings2[0][i] = strings[0][i];
+            System.out.print(strings2[0][i] + " ");
         }
+        System.out.println(" ");
+        for (int i = 0; i < strings2.length; i++) {
+
+            strings2[1][i] = strings[1][i];
+            System.out.print(strings2[1][i] + " ");
+        }
+
+//        System.out.println(" ");
+//        for (int i = 0; i < strings.length; i++) {
+//            for (int j = 0; j < strings[0].length; j++) {
+//               System.out.print(strings[i][j]+" ");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+
+//        String[][] strings2 = new String[12][17];
+//        for (int i = 0; i < strings.length; i++) {
+//            for (int j = 0; j < strings[0].length; j++) {
+//                strings2[j][i] = strings[i][j];
+//            }
+//        }
+//        for (int i = 0; i < strings2.length; i++) {
+//            for (int j = 0; j < strings2[0].length; j++) {
+//                System.out.print(strings2[i][j] + " "+i+" "+" "+j+" ");
+//            }
+//            System.out.println(" ");
+//        }
+//        int SIZE = fraseLogica.length();
+//        int length = SIZE;
+//        int width = SIZE;
+//
+//        GridPane root = new GridPane();
+//        int n = 0;
+//        for(int y = 0; y < length; y++){
+//            for(int x = 0; x < width; x++){
+//
+//                Random rand = new Random();
+//                //int rand1 = rand.nextInt(2);
+//
+//                // Create a new TextField in each Iteration
+//                TextField tf = new TextField();
+//                tf.setPrefHeight(50);
+//                tf.setPrefWidth(50);
+//                tf.setAlignment(Pos.CENTER);
+//                tf.setEditable(false);
+//                tf.setText("(" + n + ")");
+//
+//                // Iterate the Index using the loops
+//                GridPane.setRowIndex(tf,y);
+//                GridPane.setColumnIndex(tf,x);
+//                root.getChildren().add(tf);
+//                n++;
+//            }
+//            n = 0;
+//        }
+//
+//        Scene scene = new Scene(root, 500, 500);
+//        primaryStage.setTitle("Random Binary Matrix (JavaFX)");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
--- src/calculator/Controller.java
+++ src/calculator/Controller.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import Objects.TabelaVerdade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,16 +14,19 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    char[] propositions = new char[]{'p', 'q', 'r', 's'};
-    char[] connectors = new char[]{'¬','∧', 'v', '*', '→', '↔'};
+
+    private char[] propositions = new char[]{'p', 'q', 'r', 's'};
+    private char[] connectors = new char[]{'¬', '∧', 'v', '*', '→', '↔'};
 
     @FXML
     private TextField display;
+
     @FXML
     private TableView outputTable;
 
     @FXML
     private Button deleteButton;
+
     @FXML
     private Button generateButton;
 
@@ -71,7 +75,7 @@ public class Controller implements Initializable {
         } else if (event.getSource() == orButton) {
             if (permissionConnectors()) display.setText(display.getText() + "v");
         } else if (event.getSource() == xorButton) {
-            if (permissionConnectors())  display.setText(display.getText() + "*");
+            if (permissionConnectors()) display.setText(display.getText() + "*");
         } else if (event.getSource() == implicationButton) {
             if (permissionConnectors()) display.setText(display.getText() + "→");
         } else if (event.getSource() == biconditionalButton) {
@@ -81,58 +85,80 @@ public class Controller implements Initializable {
         } else if (event.getSource() == parenthesesRightButton) {
             if (permissionParenthesesRight()) display.setText(display.getText() + ")");
         } else if (event.getSource() == deleteButton) {
-            if (display.getText().length()>0) display.setText(display.getText().substring(0, (display.getText().length()-1)));
+            if (display.getText().length() > 0)
+                display.setText(display.getText().substring(0, (display.getText().length() - 1)));
         } else if (event.getSource() == generateButton) {
-            TableView tabela = new TableView<>();
+            outputTable = new TableView<>();
             char[] arrayChar = display.getText().toCharArray();
-            for (char element: arrayChar) {
+            StringBuilder fraseLogica = new StringBuilder();
+
+            for (char element : arrayChar) {
+                fraseLogica.append(element);
             }
+
+            TabelaVerdade tv = new TabelaVerdade(fraseLogica.toString());
+            String[][] matrizLogica = tv.retornaMatrizLogica();
+//            System.out.println();
+//            for (int i = 0;i< matrizLogica.length; i++)
+//                System.out.println(matrizLogica[i][0]);
+//            for (int i = 0;i< matrizLogica.length; i++)
+//                System.out.println(matrizLogica[0][i]);
+
+
+            for (int i = 0; i < fraseLogica.length(); i++) {
+                for (int j = 0; j < 17; j++) {
+                    System.out.print(matrizLogica[i][j] + " ");
+                }
+                System.out.println("");
             }
         }
+    }
 
 
-    boolean permissionPropositions(){
+    boolean permissionPropositions() {
         char[] arrayChar = display.getText().toCharArray();
-        if (arrayChar.length>0) return (new String(connectors).indexOf(arrayChar[arrayChar.length-1])!=-1||arrayChar[arrayChar.length-1]=='(');
+        if (arrayChar.length > 0)
+            return (new String(connectors).indexOf(arrayChar[arrayChar.length - 1]) != -1 || arrayChar[arrayChar.length - 1] == '(');
         else return true;
     }
 
-    boolean pemissionNegation(){
+    boolean pemissionNegation() {
         char[] arrayChar = display.getText().toCharArray();
-        if (arrayChar.length>0) return (arrayChar[arrayChar.length-1]=='(') ;
+        if (arrayChar.length > 0) return (arrayChar[arrayChar.length - 1] == '(');
         else return true;
     }
 
-    boolean permissionConnectors(){
+    boolean permissionConnectors() {
         int numConnectors = 0;
         char[] arrayChar = display.getText().toCharArray();
-        if (arrayChar.length>0){
+        if (arrayChar.length > 0) {
             for (char element : arrayChar) {
-                if (new String(connectors).indexOf(element)!=-1) numConnectors++;
+                if (new String(connectors).indexOf(element) != -1) numConnectors++;
             }
-            if (numConnectors<6){
-                return (new String(propositions).indexOf(arrayChar[arrayChar.length-1])!=-1||arrayChar[arrayChar.length-1]==')');
+            if (numConnectors < 6) {
+                return (new String(propositions).indexOf(arrayChar[arrayChar.length - 1]) != -1 || arrayChar[arrayChar.length - 1] == ')');
             }
         }
         return false;
     }
 
-    boolean permissionParenthesesLeft(){
+    boolean permissionParenthesesLeft() {
         char[] arrayChar = display.getText().toCharArray();
-        if (arrayChar.length>0) return (new String(connectors).indexOf(arrayChar[arrayChar.length-1])!=-1||arrayChar[arrayChar.length-1]=='(');
+        if (arrayChar.length > 0)
+            return (new String(connectors).indexOf(arrayChar[arrayChar.length - 1]) != -1 || arrayChar[arrayChar.length - 1] == '(');
         else return true;
     }
 
-    boolean permissionParenthesesRight(){
+    boolean permissionParenthesesRight() {
         int numParenthesesOpen = 0;
         char[] arrayChar = display.getText().toCharArray();
-        if (arrayChar.length>0){
+        if (arrayChar.length > 0) {
             for (char element : arrayChar) {
                 if (element == '(') numParenthesesOpen++;
                 if (element == ')') numParenthesesOpen--;
             }
-            if (numParenthesesOpen>0){
-                return (new String(propositions).indexOf(arrayChar[arrayChar.length-1])!=-1||arrayChar[arrayChar.length-1]==')');
+            if (numParenthesesOpen > 0) {
+                return (new String(propositions).indexOf(arrayChar[arrayChar.length - 1]) != -1 || arrayChar[arrayChar.length - 1] == ')');
             }
         }
         return false;
